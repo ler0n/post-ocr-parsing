c1aa613009ab74bd0e50e47bfb1a715de256f874
mingyu <alsrb0607@gmail.com>
2022-05-19 15:14:32 +0000

    fea) create_info.py v.00

--- data/create_info.py
+++ data/create_info.py
@@ -1,3 +1,14 @@
 from utils import *
 
-random_info()
+'''
+정보 생성 관련 내용은 메뉴얼 참고
+'''
+
+cnt = 1
+print('시작하려면 엔터를 입력하세요')
+while input()!='dd':
+    print(f'[{cnt}개째 ... ("dd" 입력 시 종료)]')
+    random_info()
+    cnt+=1
+
+print('Done')
